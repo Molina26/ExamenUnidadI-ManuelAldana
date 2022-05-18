552292aa53d17873e9b9c2bc7f972c31e88aed64
Aldo Hernández <aldohernandez@utez.edu.mx>
2022-05-18 17:22:33 -0500

    autoincrement id programing course

--- src/ProgramingCourse.kt
+++ src/ProgramingCourse.kt
@@ -19,10 +19,11 @@ class ProgramingCourse(id: Int,
     }
 
     fun addCourse (course: ProgramingCourse) : String {
-
+        var obj = this.courses.last()
         return if (course.name.length > 0 && course.description.length > 0) {
+            course.id = obj.id+1;
             this.courses.add(course)
-
+            println(this.courses.toString())
             Course.SUCCESS_REGISTER
         } else {
             Course.ERROR
@@ -44,6 +45,16 @@ class ProgramingCourse(id: Int,
     }
 
     fun updateCourse (course : ProgramingCourse) : String {
+//        var updated: Boolean = false
+//        var objCourse: ProgramingCourse
+//
+//        for (i in 0 until this.courses.size){
+//            if (this.courses.get(i).id == id){
+//                objCourse = this.courses.get(i)
+//                this.courses.remove(objCourse)
+//                delted = true
+//            }
+//        }
 
         return ""
     }
