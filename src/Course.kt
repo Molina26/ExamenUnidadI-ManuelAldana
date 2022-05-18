4a6ace896385b074d995017e40d0759c5bac5ec4
Francisco Guerrero <franciscoguerrero@utez.edu.mx>
2022-05-18 16:30:19 -0500

    delete methods CRUD

--- src/Course.kt
+++ src/Course.kt
@@ -5,8 +5,8 @@ open class Course (var id: Int, var name: String, var description: String ) {
     companion object{
             const val SUCCESS_REGISTER = "REGISTRO EXITOSO"
             const val SUCCESS_DELETE = "ELIMINACIÓN EXITOSA"
-
-            const val ERROR = "ERROR"
+            const val SUCCESS_GET = "CONSULTA EXITOSA"
+            const val ERROR = "ERROR EN EL PROCESO"
             const val INFO = "INFORMACIÓN"
 
     }
@@ -19,25 +19,6 @@ open class Course (var id: Int, var name: String, var description: String ) {
         return "El curso $nameCourse a finalizado"
     }
 
-
-    open fun getAllCourse () : MutableList<Course>? {
-        return null;
-    }
-
-    open fun addCourse ( course: Course) : String {
-
-        return "";
-    }
-
-    open fun deleteCourse (id: Int) : String {
-
-        return "";
-    }
-
-    open fun updateCourse (course : Course) : String {
-
-        return "";
-    }
 
 
 }
